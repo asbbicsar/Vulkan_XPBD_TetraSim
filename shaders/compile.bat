f39862ef65d9d158a881aca72768d846d699fe04
asbbicsar <asbbicsar@konkuk.ac.kr>
2026-04-02 21:19:14 +0900

    Removed unnecessary files

--- shaders/compile.bat
+++
@@ -1,4 +0,0 @@
-C:\VulkanSDK\1.4.328.1\Bin\glslc.exe VertexShader.vert -o vert.spv
-C:\VulkanSDK\1.4.328.1\Bin\glslc.exe FragmentShader.frag -o frag.spv
-C:\VulkanSDK\1.4.328.1\Bin\glslc.exe ComputeShader.comp -o comp.spv
-pause
